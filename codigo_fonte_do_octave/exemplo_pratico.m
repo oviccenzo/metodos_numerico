ac054d343caf9107b9277309011133eec64d3773
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-08-20 16:29:49 -0300

    Add files via upload

---
+++ codigo_fonte_do_octave/exemplo_pratico.m
@@ -0,0 +1,11 @@
+clc;
+
+matriz = [];
+linhas = input('Digite a quantidade de linha da matriz: ');
+colunas = input('Digite a quantidade de coluna da matriz: ');
+for = 1:linhas
+  for = 1:colunas
+  printf("Digite o valor do elemento %d%d da matriz: ",);
+  endfor
+endfor
+
