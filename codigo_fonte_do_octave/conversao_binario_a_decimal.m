9fffe34b48b665a98d475ec822fa57e6a7218ac5
Viccenzo de Oliveira N. Resende <128618121+oviccenzo@users.noreply.github.com>
2026-08-20 21:19:09 -0300

    Update conversao_binario_a_decimal.m

--- codigo_fonte_do_octave/conversao_binario_a_decimal.m
+++ codigo_fonte_do_octave/conversao_binario_a_decimal.m
@@ -1,2 +1,22 @@
 clc;
 
+b = [];
+
+i = 1;
+
+quociente = input('Digite o valor decimal: ');
+
+while(quociente > 0)
+
+  b(i) = mod(quociente,2);
+  quociente = floor(quociente / 2);
+  i++;
+
+endwhile
+
+n = length(b);
+printf("Binario = ");
+for i = n:-1:1
+  printf("%d",b(i));
+endfor
+printf("\n")
